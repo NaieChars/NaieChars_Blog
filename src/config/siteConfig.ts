6e6771e5f83d928c121d32d364c68123b6141824
heyuchen <3482966955@qq.com>
2026-06-11 23:05:36 +0800

    修改站点URL

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,7 +6,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 export const siteConfig: SiteConfig = {
 	title: "NaieChars",
 	subtitle: "My blog",
-	siteURL: "https://naie-char.cc/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://www.naie-char.cc/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-06-01", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	lang: SITE_LANG,
