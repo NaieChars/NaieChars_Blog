1246616a19a04d678f86aedcccbba8d140e6f44c
heyuchen <3482966955@qq.com>
2026-07-24 23:36:59 +0800

    增加神经网络文章，并对一些文章的个格式进行了调整

--- src/config/footerConfig.ts
+++ src/config/footerConfig.ts
@@ -5,7 +5,7 @@ export const footerConfig: FooterConfig = {
 	enable: true, // 是否启用Footer HTML注入功能
 	customHtml: `
   <div style="display:flex;align-items:center;gap:6px;flex-wrap:wrap;justify-content:center;font-size:14px;">
-    <img src="public/备案图标.png" alt="公安备案图标" style="width:16px;height:16px;display:inline-block;vertical-align:middle;">
+    <img src="/备案图标.png" alt="公安备案图标" style="width:16px;height:16px;display:inline-block;vertical-align:middle;">
     <a href="https://beian.mps.gov.cn/#/query/webSearch?code=51010802033357" rel="noreferrer" target="_blank">川公网安备51010802033357号</a>
     <span style="color:#999;">|</span>
     <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">蜀ICP备2026040788号-2</a>
